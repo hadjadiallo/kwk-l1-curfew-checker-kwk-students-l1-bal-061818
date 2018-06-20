1b203b1ed25c406c090a044fdb826fb8b0f9e4f2
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-20 15:27:26 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,4 +1,5 @@
 def simple_curfew_checker(time)
- if time >= 11
-  return "You're in trouble! Better get home quick!"
+  if time >= 11
+    return "You're in trouble! Better get home quick!"
+  end
 end
